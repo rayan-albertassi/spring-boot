4c9f9ffc6b6a75f3a889c73d9691c5481a44ea07
Rayan <rayan.albertassi@hotmail.com>
2026-09-03 21:16:29 -0300

    faltou getters e setters

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/NinjaModel.java
@@ -23,4 +23,28 @@ public class NinjaModel {
         this.email = email;
         this.idade = idade;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
